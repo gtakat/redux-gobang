57d953d67836cb5932246bb729addef27d0537ee
gtakat <gtakat@gmail.com>
2017-03-06 21:24:55 +0900

    test refactor

--- src/components/Masu.js
+++ src/components/Masu.js
@@ -3,11 +3,12 @@ import Goishi from './Goishi'
 
 const Masu = (props) => {
   const color = props.squares[props.row][props.col]
+  const goishi = color ? <Goishi color={color} /> : null
   return (
     <div className="App-masu"
          id={ `App-masu-${props.row}-${props.col}` }
          onClick={() => props.hitGoishi(props.row, props.col)}>
-      <Goishi color={color} />
+      {goishi}
     </div>
   )
 }
--- src/components/__tests__/Masu.test.js
+++ src/components/__tests__/Masu.test.js
@@ -5,19 +5,29 @@ import Masu from '../Masu'
 import Goishi from '../Goishi'
 
 describe('<Masu>', () => {
+  let squares = null;
+
+  beforeEach(() => {
+    squares = Array(19)
+    for (let i=0; i<squares.length; i++) {
+      squares[i] = Array(19).fill(null)
+    }
+  })
+
   it('render masu black', () => {
-    const wrapper = shallow(<Masu color="black" />)
+    squares[9][9] = "black";
+    const wrapper = shallow(<Masu squares={squares} row="9" col="9" />)
     expect(wrapper.contains(<Goishi color="black" />)).to.equal(true)
   })
 
   it('render masu white', () => {
-    const wrapper = shallow(<Masu color="white" />)
+    squares[9][9] = "white";
+    const wrapper = shallow(<Masu squares={squares} row="9" col="9" />)
     expect(wrapper.contains(<Goishi color="white" />)).to.equal(true)
   })
 
   it('render masu none', () => {
-    const wrapper = shallow(<Masu />)
-    expect(wrapper.find('.App-goishi-black').exists()).to.be.false
-    expect(wrapper.find('.App-goishi-white').exists()).to.be.false
+    const wrapper = shallow(<Masu squares={squares} row="9" col="9" />)
+    expect(wrapper.find(<Goishi />).exists()).to.be.false
   })
 })
